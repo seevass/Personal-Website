a4ee492a40166e20f4b1b5e4b1eee8f9cd8e2a18
Sebastian Cruz <scruz17@lion.lmu.edu>
2024-06-12 17:56:19 -0700

    installed mantine and established basic homepage structuring

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -1,34 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
+
+import '@mantine/core/styles.css';
+import { MantineProvider, AppShell, Burger, 
+  createTheme, Grid, SimpleGrid, 
+  Text, Space } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [opened, { toggle }] = useDisclosure();
+
+  const theme = createTheme({
+  })  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <MantineProvider theme={theme}>
+      <AppShell
+        padding="xl"
+        
+        header={{ height: 40, }}
+        navbar={{
+          width: "150",
+          breakpoint: 'sm',
+          collapsed: { mobile: !opened },
+        }}
+      >
+          <AppShell.Header>
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="md"
+            />
+          </AppShell.Header>
+
+        <AppShell.Navbar p="md">
+          <div>LOGO</div>
+          <div>Sebastian Cruz</div>
+          <Space h="xl"/>
+          <SimpleGrid cols={1}>
+            <div>About</div>
+            <div>Resume/CV</div>
+            <div>LinkedIn</div>
+            <div>GitHub</div>
+            <div>Contact</div>
+          </SimpleGrid>
+        </AppShell.Navbar>
+
+        <AppShell.Main>
+          Main
+          <Grid>
+            <Grid.Col span="auto"><div style={{ backgroundColor: 'blue', width: '100px', height: '100px' }}>
+    </div></Grid.Col>
+            <Grid.Col span={4}><div style={{ backgroundColor: 'blue', width: '100px', height: '100px' }}>
+    </div></Grid.Col>
+            <Grid.Col span="auto"><div style={{ backgroundColor: 'blue', width: '100px', height: '100px' }}>
+    </div></Grid.Col>
+          </Grid>
+        </AppShell.Main>
+
+
+
+      </AppShell>
+    </MantineProvider>
   )
 }
 
