0c093e2ca97e4ea6bdb79cca28c58e572b254fcd
Sebastian Cruz <scruz17@lion.lmu.edu>
2024-07-25 23:31:00 -0700

    adjusted padding to fit properly with screen scale

--- vite-app/src/modules/ProjectSelector.tsx
+++ vite-app/src/modules/ProjectSelector.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './ProjectSelector.css';
 import { Stack, Image } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 
 import TempImage from '../assets/flower-mural.png';
 
 function ProjectSelector() {
+  const isMobile = useMediaQuery('(max-width: 990px)');
+
+  const breakpoint = isMobile ? '10px' : '170px';
   return (
 
-    <Stack style={{paddingRight:"170px"}}>
+    <Stack style={{paddingRight:breakpoint}}>
         <Image src={TempImage} alt="flower mural" fit="fill"/>
     </Stack>
   );
